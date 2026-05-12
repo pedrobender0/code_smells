8c2f367d3db4fe3460d7163a1e1957a831ecda8f
pedrobender0 <pedrobenderprado@gmail.com>
2026-05-11 22:21:00 -0300

    Create ex_4.py

---
+++ ex_4.py
@@ -0,0 +1,17 @@
+# Code Smell: Switch Statements - A cadeia de if/elif repetia a mesma verificação de tipo
+# Solução: substituir os condicionais por um dicionário de tarifas com constantes nomeadas.
+
+tarifas_frete = {
+    "MOTO":      {"taxa_km": 2.0,  "taxa_base": 0.0},
+    "CARRO":     {"taxa_km": 3.5,  "taxa_base": 10.0},
+    "CAMINHAO":  {"taxa_km": 8.0,  "taxa_base": 50.0},
+    "BICICLETA": {"taxa_km": 1.0,  "taxa_base": 0.0},
+}
+
+class CalculadoraDeFrete:
+    @staticmethod
+    def calcular_frete(tipo_transporte, distancia):
+        if tipo_transporte not in tarifas_frete:
+            raise ValueError("Tipo de transporte desconhecido")
+        tarifa = tarifas_frete[tipo_transporte]
+        return distancia * tarifa["taxa_km"] + tarifa["taxa_base"]
